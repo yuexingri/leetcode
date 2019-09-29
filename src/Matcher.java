0f374d8347601799f8171e501f8ed9c7c8ad2ee7
yuexingri <1150082063@qq.com>
2019-09-29 18:30:07 +0800

    add #10

---
+++ src/Matcher.java
@@ -0,0 +1,21 @@
+public class Matcher {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isMatch(String s, String p) {
+        if (s.equals(p)) {
+            return true;
+        }
+        boolean isFirstMatched = false;
+        if (!s.isEmpty() && !p.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')) {
+            isFirstMatched = true;
+        }
+
+        if (!s.isEmpty() && !p.isEmpty() && p.length() > 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || (isFirstMatched && isMatch(s.substring(1), p));
+        }
+
+        return isFirstMatched && isMatch(s.substring(1), p.substring(1));
+    }
+}
