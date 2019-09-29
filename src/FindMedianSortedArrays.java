b094ba847e143a8acee3123d2d5389e25647c914
yuexingri <1150082063@qq.com>
2019-09-29 16:56:12 +0800

    #4 update

--- src/FindMedianSortedArrays.java
+++ src/FindMedianSortedArrays.java
@@ -2,65 +2,37 @@ public class FindMedianSortedArrays {
     public static void main(String[] args) {
         int[] num1 = new int[] {1, 2};
         int[] num2 = new int[] {3, 4};
-        findMedianSortedArrays(num1, num2);
+        System.out.println(findMedianSortedArrays(num1, num2));
         System.out.println();
     }
 
     private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length == 0) {
-            pickMedianSortedArrays(nums2);
-        }
-        if (nums2.length == 0) {
-            pickMedianSortedArrays(nums1);
-        }
-        int length = nums1.length + nums2.length;
-        int[] allNums = new int[length];
-        int indexForNum1 = 0;
-        int indexForNum2 = 0;
-        int indexForAll = 0;
-        boolean num1IsEnd;
-        boolean num2IsEnd;
-        while (true) {
-            num1IsEnd = arrayIsEnd(nums1, indexForNum1);
-            num2IsEnd = arrayIsEnd(nums2, indexForNum2);
-            if (num1IsEnd || num2IsEnd) {
-                break;
-            }
-            if (nums1[indexForNum1] > nums2[indexForNum2]) {
-                allNums[indexForAll] = nums2[indexForNum2];
-                indexForNum2++;
-            } else if (nums1[indexForNum1] < nums2[indexForNum2]) {
-                allNums[indexForAll] = nums1[indexForNum1];
-                indexForNum1++;
-            }
-            indexForAll++;
-        }
-        if (num1IsEnd && !num2IsEnd) {
-            putRemainToResult(nums2, indexForNum2, allNums, indexForAll);
-        }
-        if (num2IsEnd && !num1IsEnd) {
-            putRemainToResult(nums1, indexForNum1, allNums, indexForAll);
-        }
-        return pickMedianSortedArrays(allNums);
+        int i = nums1.length;
+        int j = nums2.length;
+        int k1 = (i + j + 1) / 2;
+        int k2 = (i + j + 2) / 2;
+        return (findKth(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k1)
+                + findKth(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, k2)) / 2;
     }
 
-    private static double pickMedianSortedArrays(int[] arrays) {
-        int length = arrays.length;
-        if (length % 2 == 0) {
-            return ((arrays[length/2] + arrays[length/2 - 1])) * 1.0 / 2;
+    private static double findKth(int[] num1, int start1, int end1, int[] num2, int start2, int end2, int k) {
+        int lenght1 = end1 - start1 + 1;
+        int length2 = end2 - start2 + 1;
+        if (lenght1 > length2) {
+            findKth(num2, start2, end2, num1, start1, end1, k);
+        }
+        if (lenght1 == 0) {
+            return num2[start2 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(num1[start1], num2[start2]);
+        }
+        int i = start1 + Math.min(lenght1, k/2) - 1;
+        int j = start2 + Math.min(length2, k/2) - 1;
+        if (num1[i] > num2[j]) {
+            return findKth(num1, start1, end1, num2, j + 1, end2, k - (j - start2 + 1));
         } else {
-            return arrays[(length - 1)/2] * 1.0;
+            return findKth(num1, i + 1,end1, num2, start2, end2, k - (i - start1 + 1));
         }
-    }
-
-    private static void putRemainToResult(int[] source, int sourceIndex, int[] target, int targetIndex) {
-        for (int i = sourceIndex; i < source.length - sourceIndex; i++) {
-            target[targetIndex] = source[i];
-            targetIndex++;
-        }
-    }
-
-    private static boolean arrayIsEnd(int[] nums, int index) {
-        return index == nums.length;
     }
 }
